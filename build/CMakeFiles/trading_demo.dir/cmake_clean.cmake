88ebb2d832416d2b6cde0c3d50424c5c96c2c865
HN026 <huzaifanaseer026@gmail.com>
2025-09-14 15:54:28 +0530

    Added README

---
+++ build/CMakeFiles/trading_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trading_demo.dir/link.d"
+  "CMakeFiles/trading_demo.dir/examples/trading_demo.cpp.o"
+  "CMakeFiles/trading_demo.dir/examples/trading_demo.cpp.o.d"
+  "trading_demo"
+  "trading_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trading_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
